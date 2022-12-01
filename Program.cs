bdb50736fcb6bdb198f657a4d03953e816c8fb6a
misha-solntcev <misha.solntcev@gmail.com>
2022-12-02 00:34:53 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 8. Найти все целые числа из промежутка от 200 до 500, 
+    у которых ровно шесть делителей. */
+
+namespace Lab_3_2_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 500; i >= 200; i--)
+            {
+                int count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 6)
+                    Console.WriteLine($"i={i}, count={count}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
